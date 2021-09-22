1f2d5e2b662b0f91564ecb2e809198823b6ec79d
jishu-yadav <54631311+jishu-yadav@users.noreply.github.com>
2021-09-23 00:46:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
+
+
+const db = mysql.createConnection({
+  host: "test4.czdxirnzvqzn.us-east-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "321Jishu",
+  database: "TESTDB4",
+});
+
+db.connect((err) => {
+  if (!err) {
+    console.log("DB connection succeded");
+  } else {
+    console.log("connection failed");
+  }
+});
+
+
+
+
 // Parsing middleware
 // Parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }));
