3bf9e8c44c8443f66234ea12d23247a67f3dac4b
jishu-yadav <54631311+jishu-yadav@users.noreply.github.com>
2021-11-23 22:50:07 +0530

    Add files via upload

---
+++ lab4/commands.sh
@@ -0,0 +1,5 @@
+aws s3 cp s3://bucket191998/About.html /var/www/html/
+aws s3 cp s3://bucket191998/Error.html /var/www/html/
+aws s3 cp s3://bucket191998/homepage.html /var/www/html/
+aws s3 cp s3://bucket191998/kinds.html /var/www/html/
+aws s3 cp s3://bucket191998/Types.html /var/www/html/
